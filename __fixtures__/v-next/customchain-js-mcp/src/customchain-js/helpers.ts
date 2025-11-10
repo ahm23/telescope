d3d6944a800e954c3a2998ae65d81b55933ec7cb
Wonhee Lee <2wheeh@gmail.com>
2025-11-11 01:19:23 +0900

    fix: prevent globalThis hoisting override in ESM

--- __fixtures__/v-next/customchain-js-mcp/src/customchain-js/helpers.ts
+++ __fixtures__/v-next/customchain-js-mcp/src/customchain-js/helpers.ts
@@ -8,7 +8,7 @@
 declare var self: any | undefined;
 declare var window: any | undefined;
 declare var global: any | undefined;
-var globalThis: any = (() => {
+const _globalThis: any = (() => {
   if (typeof globalThis !== 'undefined') return globalThis;
   if (typeof self !== 'undefined') return self;
   if (typeof window !== 'undefined') return window;
@@ -17,8 +17,8 @@ var globalThis: any = (() => {
 })();
 
 const atob: (b64: string) => string =
-  globalThis.atob ||
-  ((b64) => globalThis.Buffer.from(b64, 'base64').toString('binary'));
+  _globalThis.atob ||
+  ((b64) => _globalThis.Buffer.from(b64, 'base64').toString('binary'));
 
 export function bytesFromBase64(b64: string): Uint8Array {
   const bin = atob(b64);
@@ -30,8 +30,8 @@ export function bytesFromBase64(b64: string): Uint8Array {
 }
 
 const btoa: (bin: string) => string =
-  globalThis.btoa ||
-  ((bin) => globalThis.Buffer.from(bin, 'binary').toString('base64'));
+  _globalThis.btoa ||
+  ((bin) => _globalThis.Buffer.from(bin, 'binary').toString('base64'));
 
 export function base64FromBytes(arr: Uint8Array): string {
   const bin: string[] = [];

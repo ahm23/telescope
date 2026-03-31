e2b2e7615b3e864ff92088d8e8f746da691d7419
Wonhee Lee <2wheeh@gmail.com>
2026-03-31 15:12:02 +0900

    test: add smoke test for bin entry point

---
+++ packages/telescope/__tests__/bin.test.ts
@@ -0,0 +1,12 @@
+import { existsSync } from 'fs';
+import { resolve } from 'path';
+import pkg from '../package.json';
+
+describe('bin entry point', () => {
+  const binEntries = Object.entries(pkg.bin);
+
+  it.each(binEntries)('%s should exist in dist after build', (_, file) => {
+    const binPath = resolve(__dirname, '../dist', file);
+    expect(existsSync(binPath)).toBe(true);
+  });
+});
